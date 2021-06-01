be47a78b466cb1aeec4897f9723863309edc8dfe
SearchLee <1402583679@qq.com>
2021-06-01 21:41:00 +0800

    [New features]：确定了项目文件结构

---
+++ WebViewApp/app/src/main/java/com/example/webviewapp/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.webviewapp.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.webviewapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
