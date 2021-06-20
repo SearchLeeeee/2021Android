5d48778a65fdfe867c1e3c3fadde0925bf7ec655
SearchLee <1402583679@qq.com>
2021-06-20 18:00:28 +0800

    [update]：完善视频播放问题，界面未弄好看

--- WebViewApp/app/src/main/java/com/example/webviewapp/ui/activity/VideoViewActivity.java
+++ WebViewApp/app/src/main/java/com/example/webviewapp/ui/activity/VideoViewActivity.java
@@ -1,6 +1,5 @@
 package com.example.webviewapp.ui.activity;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.MediaController;
@@ -23,8 +22,10 @@ public class VideoViewActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         uri = getIntent().getStringExtra("video");
+        //TODO:测试用uri
+        uri = "https://media.w3.org/2010/05/sintel/trailer.mp4";
         Log.d(TAG, "onCreate: " + uri);
-        viewBinding.videoView.setVideoURI(Uri.parse(uri));
+        viewBinding.videoView.setVideoPath(uri);
         MediaController mediaController = new MediaController(this);
         mediaController.setMediaPlayer(viewBinding.videoView);
         viewBinding.videoView.setMediaController(mediaController);
